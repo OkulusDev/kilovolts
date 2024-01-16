857aae1e6698d9e5c800ecfd1e2a6246be4911ce
okulus <bro.alexeev@inbox.ru>
2024-01-16 22:15:17 +0700

    feat: add photoresistor, inductor, polarized capacitor, potentiometer, capacitor, upgrade code

--- kilovolts.py
+++ kilovolts.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 """Kilovolts - An open, fast and simple python PCB emulator
 Copyright (C) Alexeev Bronislav, 2024"""
-from modules.linear_components import Resistor
+from modules.linear_components import Resistor, Capacitor, Inductor, \
+                                        Potentiometer, Photoresistor, \
+                                        PolarizedCapacitor
 from modules.transistors import Transistor
 from modules.pcb import PCB
 
@@ -13,10 +15,30 @@ r1 = Resistor('R1', 100.0, 9.0, 0.1)
 pcb.add_component(r1)
 t1 = Transistor('T1', 'NPN', 0.7, 0.1, 1.2, 2.0)
 pcb.add_component(t1)
+c1 = Capacitor('C1', 10e-6, 0.8, 0.1)
+pcb.add_component(c1)
+i1 = Inductor('I1', 1e-3, 0.2, 3.4)
+pcb.add_component(i1)
+pm1 = Potentiometer('PM1', 0.2, 0.3, 1.3)
+pcb.add_component(pm1)
+p1 = Photoresistor('P1', 1.2, 3.1, 9.0)
+pcb.add_component(p1)
+pc1 = PolarizedCapacitor('PC1', True, 100e-6, 2.0, 3.3)
+pcb.add_component(pc1)
 # Соединияем компоненты проводом
 pcb.connect_components(r1, t1)
+pcb.connect_components(r1, c1)
+pcb.connect_components(c1, t1)
+pcb.connect_components(r1, i1)
+pcb.connect_components(i1, t1)
+pcb.connect_components(r1, pc1)
+pcb.connect_components(i1, pm1)
+pcb.connect_components(p1, pm1)
+pcb.connect_components(pc1, p1)
 
 # Выводим имена компонентов и информацию о PCB
 print(r1.get_name())
 print(t1.get_name())
+print(c1.get_name())
+print(i1.get_name())
 print(pcb.get_info())
--- modules/linear_components.py
+++ modules/linear_components.py
@@ -48,3 +48,83 @@ class Resistor(LinearComponent):
             new_res: float - новое значение сопротивления"""
         self.resistance = new_res
 
+
+class Capacitor(LinearComponent):
+    """Конденсатор - это электронный компонент, способный накапливать и 
+    хранить электрический заряд. Он состоит из двух проводящих пластин,
+    разделенных изолятором (диэлектриком). Конденсаторы имеют емкость,
+    измеряемую в фарадах (Ф). Большая емкость означает большую способность
+    конденсатора накапливать заряд."""
+    def __init__(self, name: str, capacitance=10e-6, voltage: float=0.0,
+                amperage: float=0.0, wired_objects: list=[]):
+        super().__init__(name, 'Capacitor', voltage, amperage, wired_objects)
+        self.capacitance = capacitance
+
+    def change_capicitance(self, new_cap: float):
+        self.capacitance = new_cap
+
+
+class Inductor(LinearComponent):
+    """Индуктор - это электронный компонент, использующийся для создания и
+    хранения магнитного поля. Он состоит из проводящей катушки, обычно
+    изготовленной из провода, намотанного вокруг магнитопровода. Индукторы
+    обладают индуктивностью, измеряемой в генри (Гн). Большая индуктивность
+    означает большую способность индуктора создавать магнитное поле."""
+    def __init__(self, name: str, inductance=1e-3, voltage: float=0.0,
+                 amperage: float=0.0, wired_objects: list=[]):
+        super().__init__(name, 'Inductor', voltage, amperage, wired_objects)
+        self.inductance = inductance
+
+    def change_inductance(self, new_ind):
+        self.inductance = new_ind
+
+
+class Potentiometer(LinearComponent):
+    """Потенциометр - это переменный резистор с тремя выводами, который
+    позволяет изменять сопротивление в зависимости от положения подвижного
+    контакта. Потенциометры широко используются для регулировки напряжения или
+    уровня сигнала в электрических цепях."""
+    def __init__(self, name: str, resistance: float, voltage: float=0.0,
+                 amperage: float=0.0, wired_objects: list=[]):
+        super().__init__(name, 'Potentiometer', voltage, amperage, wired_objects)
+        self.resistance = resistance
+
+    def change_resistance(self, new_res: float):
+        self.resistance = new_res
+
+
+class PolarizedCapacitor(LinearComponent):
+    """Поляризованный конденсатор - это тип конденсатора, который имеет
+    полярность, то есть он должен быть подключен в цепь с учетом правильной
+    полярности. Он обычно используется для фильтрации постоянного напряжения или
+    для создания временных задержек в электрических цепях. Поляризованные
+    конденсаторы имеют положительный и отрицательный выводы, и их емкость
+    измеряется в микрофарадах (мкФ) или пикофарадах (пФ)."""
+    def __init__(self, name: str, polarity: bool, capacitance: float=100e-6,
+                 voltage: float=0.0, amperage=0.0, wired_objects: list=[]):
+        super().__init__(name, 'Polarized Capacitor', voltage, amperage,
+                        wired_objects)
+        self.capacitance = capacitance
+        self.polarity = polarity
+
+    def change_capicitance(self, new_cap: float):
+        self.capacitance = new_cap
+
+    def change_polarity(self, new_pol: bool):
+        self.polarity = new_pol
+
+
+class Photoresistor(LinearComponent):
+    """Фоторезистор, также известный как фотоэлектрический резистор или LDR
+    (Light-Dependent Resistor), - это резистор, чье сопротивление изменяется в
+    зависимости от интенсивности света, падающего на него. Он используется для
+    обнаружения или измерения освещенности в различных приложениях, таких как
+    автоматические световые выключатели или фотореле."""
+    def __init__(self, name: str, resistance: float, voltage: float=0.0,
+                 amperage: float=0.0, wired_objects: list=[]):
+        super().__init__(name, 'Photoresistor', voltage, amperage, wired_objects)
+        self.resistance = resistance
+
+    def change_resistance(self, new_res: float):
+        self.resistance = new_res
+
